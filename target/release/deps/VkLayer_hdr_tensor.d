28af88694678bef87735742998e6ebe26412eb38
JOCKER3201 <michaelfurtak@outlook.com>
2026-03-19 12:04:28 +0100

    fix performance for rtx

--- target/release/deps/VkLayer_hdr_tensor.d
+++ target/release/deps/VkLayer_hdr_tensor.d
@@ -1,6 +1,7 @@
-/home/michael/.git/autohdr/target/release/deps/VkLayer_hdr_tensor.d: src/lib.rs src/shader.rs
+/home/michael/.git/autohdr/target/release/deps/VkLayer_hdr_tensor.d: src/lib.rs src/shader.rs src/../shader.spv
 
-/home/michael/.git/autohdr/target/release/deps/libVkLayer_hdr_tensor.so: src/lib.rs src/shader.rs
+/home/michael/.git/autohdr/target/release/deps/libVkLayer_hdr_tensor.so: src/lib.rs src/shader.rs src/../shader.spv
 
 src/lib.rs:
 src/shader.rs:
+src/../shader.spv:
--- target/release/libVkLayer_hdr_tensor.d
+++ target/release/libVkLayer_hdr_tensor.d
@@ -1,1 +1,1 @@
-/home/michael/.git/autohdr/target/release/libVkLayer_hdr_tensor.so: /home/michael/.git/autohdr/src/lib.rs /home/michael/.git/autohdr/src/shader.rs
+/home/michael/.git/autohdr/target/release/libVkLayer_hdr_tensor.so: /home/michael/.git/autohdr/shader.spv /home/michael/.git/autohdr/src/lib.rs /home/michael/.git/autohdr/src/shader.rs
